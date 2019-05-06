e0e6afec92ad36c72774c86ba3ea57455d002cb3
martinzuma <49009583+martinzuma@users.noreply.github.com>
2019-05-05 21:14:47 -0300

    Primera beta pública

---
+++ precache-manifest.e4a6fb05f53efc5aca3b2f1aab8866a1.js
@@ -0,0 +1,302 @@
+self.__precacheManifest = [
+  {
+    "revision": "ae588946014f513b393d",
+    "url": "/css/app.c862afcd.css"
+  },
+  {
+    "revision": "ae588946014f513b393d",
+    "url": "/js/app.abc6d33f.js"
+  },
+  {
+    "revision": "a75d62b383522a04d027",
+    "url": "/css/chunk-01185838.fa4cfaca.css"
+  },
+  {
+    "revision": "a75d62b383522a04d027",
+    "url": "/js/chunk-01185838.3fc78c7f.js"
+  },
+  {
+    "revision": "b78a11fa93539b9315b0",
+    "url": "/css/chunk-02c5cbb8.47d72738.css"
+  },
+  {
+    "revision": "b78a11fa93539b9315b0",
+    "url": "/js/chunk-02c5cbb8.7d73221e.js"
+  },
+  {
+    "revision": "29935a20a535b2591491",
+    "url": "/css/chunk-04d6ef72.c6e88252.css"
+  },
+  {
+    "revision": "29935a20a535b2591491",
+    "url": "/js/chunk-04d6ef72.7df6e847.js"
+  },
+  {
+    "revision": "6f31e41e214b09fd8b44",
+    "url": "/css/chunk-0857f596.50098436.css"
+  },
+  {
+    "revision": "6f31e41e214b09fd8b44",
+    "url": "/js/chunk-0857f596.b25d4cc1.js"
+  },
+  {
+    "revision": "56f6391eae405942ba04",
+    "url": "/css/chunk-16e5c836.a2aa8904.css"
+  },
+  {
+    "revision": "56f6391eae405942ba04",
+    "url": "/js/chunk-16e5c836.8c4db38b.js"
+  },
+  {
+    "revision": "550271bbc24db8862d5c",
+    "url": "/css/chunk-1703e704.47d72738.css"
+  },
+  {
+    "revision": "550271bbc24db8862d5c",
+    "url": "/js/chunk-1703e704.5d4f1fe9.js"
+  },
+  {
+    "revision": "003fd1d85439d8aeeb1b",
+    "url": "/js/chunk-3131ce48.9217d4f9.js"
+  },
+  {
+    "revision": "27625f3930ea9f095a80",
+    "url": "/css/chunk-4d56a6a6.2cde5b21.css"
+  },
+  {
+    "revision": "27625f3930ea9f095a80",
+    "url": "/js/chunk-4d56a6a6.edbec2f6.js"
+  },
+  {
+    "revision": "f89731b14176cabb6d74",
+    "url": "/css/chunk-62fca481.e0dd4503.css"
+  },
+  {
+    "revision": "f89731b14176cabb6d74",
+    "url": "/js/chunk-62fca481.e92b3f9c.js"
+  },
+  {
+    "revision": "7e9be9fa030ed4bfa112",
+    "url": "/css/chunk-bcbef49e.9c241cd0.css"
+  },
+  {
+    "revision": "7e9be9fa030ed4bfa112",
+    "url": "/js/chunk-bcbef49e.9e8e33c6.js"
+  },
+  {
+    "revision": "c107b830a2c546b98332",
+    "url": "/css/chunk-df82346a.2aa35064.css"
+  },
+  {
+    "revision": "c107b830a2c546b98332",
+    "url": "/js/chunk-df82346a.03d401a9.js"
+  },
+  {
+    "revision": "52364fa81d0f58f5c84f",
+    "url": "/css/chunk-vendors.be6fe9eb.css"
+  },
+  {
+    "revision": "52364fa81d0f58f5c84f",
+    "url": "/js/chunk-vendors.31441730.js"
+  },
+  {
+    "revision": "519f70a2b1ca33c59db0f7c568d672f1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6582dc5e6a58474609265358e8a7f316",
+    "url": "/assets/json/calendario/data.json"
+  },
+  {
+    "revision": "eab7ccbff2f9bd8f91374a8a144bc5f6",
+    "url": "/assets/json/calendario/refs.json"
+  },
+  {
+    "revision": "0ceb3fe7d29debf28d42b7310a56af1c",
+    "url": "/assets/img/iso.png"
+  },
+  {
+    "revision": "520e7300eb8a47a5ec521a429211c168",
+    "url": "/assets/json/planes/fi_Bioingenieria.json"
+  },
+  {
+    "revision": "49a33b89a9bd1132b5d3f3530f343456",
+    "url": "/assets/json/horarios/fi_Bioingenieria.json"
+  },
+  {
+    "revision": "8971553b81ca09dd6d92159aa9f4e024",
+    "url": "/assets/json/planes/fi_IngenieriaAgronomica.json"
+  },
+  {
+    "revision": "48082795c906e36ad744e1253c484caa",
+    "url": "/assets/json/planes/fi_IngenieriaDeMinas.json"
+  },
+  {
+    "revision": "962b4dc24991c157bb85d65a5f51d4c6",
+    "url": "/assets/json/planes/fi_IngenieriaElectrica.json"
+  },
+  {
+    "revision": "9883d432d599075e480d035c01fa4513",
+    "url": "/assets/json/planes/fi_IngenieriaCivil.json"
+  },
+  {
+    "revision": "1eeeb5cc7a5adddeac25b6a1d1c87139",
+    "url": "/assets/json/planes/fi_IngenieriaElectromecanica.json"
+  },
+  {
+    "revision": "f8938e1d140f6446cf9a96d924c6ce67",
+    "url": "/assets/json/planes/fi_IngenieriaEnAgrimensura.json"
+  },
+  {
+    "revision": "84c089ca4b71f2e2b985873e11b7f2b5",
+    "url": "/assets/json/planes/fi_IngenieriaElectronica.json"
+  },
+  {
+    "revision": "ce25e5c955b3a52a823c2d0d91c843d5",
+    "url": "/assets/json/planes/fi_IngenieriaEnAlimentos.json"
+  },
+  {
+    "revision": "a5a286230d5425fd478f78f05191b325",
+    "url": "/assets/json/planes/fi_IngenieriaEnMetalurgiaExtractiva.json"
+  },
+  {
+    "revision": "46b1971604820509af79210931547113",
+    "url": "/assets/json/planes/fi_IngenieriaIndustrial.json"
+  },
+  {
+    "revision": "2b6bf58be5af582006cff59e2474def5",
+    "url": "/assets/json/planes/fi_IngenieriaMecanica.json"
+  },
+  {
+    "revision": "c6e1b122893dad7016c53fc181286a61",
+    "url": "/assets/json/planes/fi_IngenieriaQuimica.json"
+  },
+  {
+    "revision": "ca8a3085e9ccadc571fb7b179e736117",
+    "url": "/assets/json/carreras/fi.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "d41d8cd98f00b204e9800998ecf8427e",
+    "url": "/service-worker.js"
+  },
+  {
+    "revision": "d0b0614abfcc1e2305f1eae4fc3ff638",
+    "url": "/assets/svg/logo_facso.svg"
+  },
+  {
+    "revision": "d6f4cf3e6ce42764c00349a732c5ffc1",
+    "url": "/assets/svg/logo_eucs.svg"
+  },
+  {
+    "revision": "65fea9039a8351d2ea5f6d89777526ca",
+    "url": "/assets/svg/logo_ffha.svg"
+  },
+  {
+    "revision": "75a64adc056c9bb0cb9cf8f92dc270b0",
+    "url": "/assets/svg/logo_fcefn.svg"
+  },
+  {
+    "revision": "e9baeb0ee5f243f8ebfc369bfb6d51dd",
+    "url": "/assets/svg/logo_fi.svg"
+  },
+  {
+    "revision": "d1a05ff142405a2145a408a86a489f76",
+    "url": "/assets/fonts/Effra-Bold.woff2"
+  },
+  {
+    "revision": "fbfdc1e49bfa893b6e309b820e209876",
+    "url": "/assets/fonts/Effra-BoldItalic.woff2"
+  },
+  {
+    "revision": "0b38cc0e79f1976569e13dfb1f8bc9d2",
+    "url": "/assets/fonts/Effra-Italic.woff2"
+  },
+  {
+    "revision": "aca251fb384bb7035f139ede03251335",
+    "url": "/assets/fonts/Effra-Regular.woff2"
+  },
+  {
+    "revision": "c9c56c7a213e72d201acc4d67b0e47f4",
+    "url": "/assets/fonts/EffraHeavy-Italic.woff2"
+  },
+  {
+    "revision": "4cd95cc10d4bf17ae86811df77f6978b",
+    "url": "/assets/fonts/EffraHeavy-Regular.woff2"
+  },
+  {
+    "revision": "886bf3b8cd1582102ecd8ec49d01bc5e",
+    "url": "/assets/fonts/EffraLight-Italic.woff2"
+  },
+  {
+    "revision": "e8b9d831a49cce88a1fe829686a67c49",
+    "url": "/assets/fonts/EffraLight-Regular.woff2"
+  },
+  {
+    "revision": "c19fcf3fd9e4f25508300f5ddb5e9098",
+    "url": "/assets/fonts/EffraMedium-Italic.woff2"
+  },
+  {
+    "revision": "5855d97855b754741d63828bf36578a9",
+    "url": "/assets/fonts/EffraMedium-Regular.woff2"
+  },
+  {
+    "revision": "398d6f1f31b04e3a218f5f4922122a88",
+    "url": "/assets/svg/logo_faud.svg"
+  },
+  {
+    "revision": "69bd0bcdeedcdeb4fde4a05b06e85131",
+    "url": "/img/icon.png"
+  },
+  {
+    "revision": "c2d6d528fe132629b96b9777129e84b4",
+    "url": "/assets/fonts/Effra-Bold.woff"
+  },
+  {
+    "revision": "776e5c057ad3b571c04f31fdbe2c0eef",
+    "url": "/assets/fonts/Effra-BoldItalic.woff"
+  },
+  {
+    "revision": "010a75a5d5956a399e333e32a0709218",
+    "url": "/assets/fonts/Effra-Italic.woff"
+  },
+  {
+    "revision": "94f05bd4d14673d91a05b84c5e8da856",
+    "url": "/assets/fonts/EffraHeavy-Italic.woff"
+  },
+  {
+    "revision": "a42a8c6f27783d3009314673b5a78cee",
+    "url": "/assets/fonts/EffraHeavy-Regular.woff"
+  },
+  {
+    "revision": "04d9ed7d793c354215bc2492a9381d31",
+    "url": "/assets/fonts/EffraLight-Italic.woff"
+  },
+  {
+    "revision": "75d67d071c2f18638e1d916296200297",
+    "url": "/assets/fonts/EffraLight-Regular.woff"
+  },
+  {
+    "revision": "8f664e4aaae9135a338be7cee9391c32",
+    "url": "/assets/fonts/EffraMedium-Italic.woff"
+  },
+  {
+    "revision": "a2767c7e1d736a0d254d9106bf476806",
+    "url": "/assets/fonts/EffraMedium-Regular.woff"
+  },
+  {
+    "revision": "02d10ab5c7467d8cb7b03308a9de2ee1",
+    "url": "/assets/fonts/Effra-Regular.woff"
+  },
+  {
+    "revision": "bfb8519ec72bbe2d974d0c4024073fe7",
+    "url": "/img/logo.png"
+  },
+  {
+    "revision": "6ee00f3a1d46c32aedf757cb9d3b40ca",
+    "url": "/img/logo_fi.png"
+  }
+];
---
+++ service-worker.js
@@ -0,0 +1,3 @@
+importScripts("/precache-manifest.e4a6fb05f53efc5aca3b2f1aab8866a1.js", "https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+
